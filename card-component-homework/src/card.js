a25128c1c1e2a2ac56e1e538f1bc859f028de3c3
sahildev98 <sahil.98@hotmail.com>
2023-09-19 11:25:26 +0100

    Added image of harry poter book

--- card-component-homework/src/card.js
+++ card-component-homework/src/card.js
@@ -1,7 +1,7 @@
 function Card(){
     return (
         <section className="Card-component">
-            <img src=""/>
+            <img src="https://m.media-amazon.com/images/I/513TQ4ihqqL.jpg"/>
             <p>Harry Potter and the Goblet of Fire </p>
             <p>Jk Rowling</p>
             <p>Made in year 2000</p>
