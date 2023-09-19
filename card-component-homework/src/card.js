41134ef22d286852f45821f2cea4f7047d7ff298
sahildev98 <sahil.98@hotmail.com>
2023-09-19 11:24:34 +0100

    Added card component elements inside card.js

---
+++ card-component-homework/src/card.js
@@ -0,0 +1,10 @@
+function Card(){
+    return (
+        <section className="Card-component">
+            <img src=""/>
+            <p>Harry Potter and the Goblet of Fire </p>
+            <p>Jk Rowling</p>
+            <p>Made in year 2000</p>
+        </section>
+    )
+}
