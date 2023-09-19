1692e462c740334ba05d37c348ca6aa544d29f35
sahildev98 <sahil.98@hotmail.com>
2023-09-19 10:51:46 +0100

    Added navigation and exported the function to add into app.js

---
+++ card-component-homework/src/navbar.js
@@ -0,0 +1,12 @@
+export default function Navbar(){
+    return <>
+    <Header/>
+    <nav className="nav">
+          <ul>
+            <li><a href='libaries.html'>Libaries nearby</a></li>
+            <li><a href='about.html'>About</a></li>
+            <li><a href='Projects.html'>Contact</a></li>
+          </ul>
+        </nav>
+        </>
+}
