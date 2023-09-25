9ebcbf259acfad44ad6b3e77b5a73fec4685e09a
sahil <sahil.98@hotmail.com>
2023-09-25 18:44:20 +0100

    Added props elemnts into the card

--- card-component-homework/src/App.js
+++ card-component-homework/src/App.js
@@ -1,17 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-import Navbar from './navbar.js'
-import Card from './card.js'
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./navbar.js";
+import Card from "./card.js";
 
 function App() {
   return (
     <>
-    <header>
-    <Navbar/>
-    </header>
-    <main>
-    <Card/>
-    </main>
+      <header>
+        <Navbar />
+      </header>
+      <main>
+        <Card
+          img="https://m.media-amazon.com/images/I/513TQ4ihqqL.jpg"
+          name="Harry Potter and the Goblet of Fire"
+          author="Jk Rowling"
+          publishDate="Made in year 2000"
+        />
+      </main>
     </>
   );
 }
