819ff146cc4f09615dc3cf9484202b13120dcd18
sahil <sahil.98@hotmail.com>
2023-10-28 19:21:51 +0100

    Added the correct date

--- card-component-homework/src/App.js
+++ card-component-homework/src/App.js
@@ -20,7 +20,7 @@ function App() {
           img="https://m.media-amazon.com/images/I/81RkTjE-tIL._SY466_.jpg"
           name="Percy Jackson and The Lightning Thief"
           author="Rick Riordan"
-          publishDate="Made in year 2013"
+          publishDate="Made in year 2005"
         />
          <Card
           img="https://m.media-amazon.com/images/I/513TQ4ihqqL.jpg"
