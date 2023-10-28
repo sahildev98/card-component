b5de77b0d7f26598da262356d0387e6aa4542c75
sahil <sahil.98@hotmail.com>
2023-10-28 19:32:29 +0100

    Added more percy jackson books to card compoenents

--- card-component-homework/src/App.js
+++ card-component-homework/src/App.js
@@ -23,15 +23,15 @@ function App() {
           publishDate="Made in year 2005"
         />
          <Card
-          img="https://m.media-amazon.com/images/I/513TQ4ihqqL.jpg"
-          name="Harry Potter and the Goblet of Fire"
-          author="Jk Rowling"
-          publishDate="Made in year 2000"
+          img="https://m.media-amazon.com/images/I/91ED1cwnc5L._SY466_.jpg"
+          name="Percy Jackson and The Sea of Monsters"
+          author="Rick Riordan"
+          publishDate="Made in year 2006"
         /> <Card
-          img="https://m.media-amazon.com/images/I/513TQ4ihqqL.jpg"
-          name="Harry Potter and the Goblet of Fire"
-          author="Jk Rowling"
-          publishDate="Made in year 2000"
+          img="https://m.media-amazon.com/images/I/51o3Gno4PPL._SY445_SX342_.jpg"
+          name="Percy Jackson and The Titan's Curse"
+          author="Rick Riordan"
+          publishDate="Made in year 2007"
         /> <Card
           img="https://m.media-amazon.com/images/I/513TQ4ihqqL.jpg"
           name="Harry Potter and the Goblet of Fire"
